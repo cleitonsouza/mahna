778ef6341dd4461a909c33a6d11e46ba3d43d503
Cleiton Souza <cleiton7souza@gmail.com>
2024-01-24 23:02:18 -0300

    projeto atualizado e finalizado

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -8,7 +8,6 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -23,6 +22,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
+
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
@@ -33,133 +33,138 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-$(document).ready(function(){
-      // Exemplo de JSON com 3 objetos
-      var jsonData = [
+$(document).ready(function() {
+    var jsonData = [
         {
             slug: "acupuntura", 
-            title: "Acupuntura", 
-            description: "<img class=\"img-fluid mb-5\" src=\"http://www.espacomahna.com.br/img/em_img_acupuntura.jpg\" /><p>Consiste na aplicação de agulhas bem finas, em pontos específicos do corpo, para melhorar a imunidade e tratar problemas emocionais e doenças físicas.</p>"
+            title: "Acupuntura",
+            image: "acupuntura.jpg",
+            description: "<p>Consiste na aplicação de agulhas bem finas, em pontos específicos do corpo, para melhorar a imunidade e tratar problemas emocionais e doenças físicas.</p>"
         },
         {
             slug: "auriculoterapia", 
             title: "Auriculoterapia", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            image: "auriculoterapia.jpeg",
+            description: "<p>Amplamente utilizada na assistência à saúde, nos aspectos preventivos e curativos. Trabalha com a estimulação ou sedação de pontos na orelha para aliviar dores e/ou tratar problemas físicos e psíquicos.</p>"
         },
         {
             slug: "barras-de-access", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Barras de Access", 
+            image: "barras-de-access.jpg",
+            description: "<p>Indicada para todas as idades e momentos da vida, é uma ferramenta terapêutica para facilitar as mudanças desejadas, destruindo os pontos de vista fixo e barreiras para que essas mudanças finalmente aconteçam e você viva a sua realidade desejada.</p>"
         },
         {
             slug: "drenagem-linfatica", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Drenagem Linfática", 
+            image: "drenagem-linfatica.jpeg",
+            description: "<p>Técnica de massagem que estimula o sistema linfático a trabalhar em um ritmo mais acelerado, eliminando o excesso de líquido e toxinas do organismo.<br/><br/>Também estimula a regeneração dos tecidos, melhora o sistema imunológico, é relaxante e tranquilizante, combate a celulite e a gordura localizada e ainda melhora a ação anti-inflamatória do organismo.</p>"
         },
         {
             slug: "florais-de-bach", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Florais de Bach", 
+            image: "florais-de-bach.jpeg",
+            description: "<p>Os florais de Bach é um sistema de terapia natural composto por 38 essências de flores. Durante a terapia, é trabalhado com uma escuta abrangente para entender o que está além da queixa que a pessoa traz e, para cada situação apresentada, é possível combinar diferentes essências do floral para ter um resultado mais adequado.</p>"
         },
         {
             slug: "massagem-ayurvedica", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Massagem Ayurvédica", 
+            image: "massagem-ayurvedica.jpeg",
+            description: "<p>Massagem indiana que visa o reequilíbrio físico mental e energético, trazendo melhoras ao funcionamento do organismo, atuando principalmente nos sistemas nervoso, muscular, linfático e circulatório, além de induzir nosso organismo a um profundo relaxamento.</p>"
         },
         {
             slug: "massagem-lomi-lomi", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Massagem Lomi-Lomi", 
+            image: "massagem-lomi-lomi.jpeg",
+            description: "<p>Massagem tradicional havaiana é uma massagem relaxante que trabalha o corpo sem usar os dedos. Diferentemente do método oriental, que se baseia em pontos de pressão, a técnica é chamada de circulatória, uma vez que move os fluidos corporais para deixar a energia circular e aliviar a tensão.</p>"
         },
         {
             slug: "massagem-modeladora", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Massagem Modeladora", 
+            image: "massagem-modeladora.jpeg",
+            description: "<p>Técnica que é feita com movimentos mais fortes e profundos, com o intuito de atingir camadas mais profundas da pele e atuar também na circulação, melhorando o metabolismo da região, e sendo eficiente contra a celulite.</p>"
         },
         {
             slug: "massagem-relaxante", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Massagem Relaxante", 
+            image: "massagem-relaxante.jpeg",
+            description: "<p>Usando-se de movimentos leves, é uma excelente alternativa para o tratamento de dores musculares, promovendo o bem-estar e aliviando o desconforto das regiões mais afetadas.</p>"
         },
         {
             slug: "moxaterapia", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Moxaterapia", 
+            image: "moxaterapia.jpg",
+            description: "<p>Técnica que baseia-se nos mesmos princípios e conhecimentos dos meridianos de energia trabalhados na acupuntura, sendo um recurso valioso para tratamentos, principalmente da dor.</p>"
         },
         {
-            slug: "reflexologia", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            slug: "reflexologia",
+            title: "Reflexologia", 
+            image: "reflexologia.jpeg",
+            description: "<p>Técnica de massagem que utiliza a pressão em determinados pontos dos pés ou das mãos, os quais correspondem a regiões e órgãos do corpo humano.<br/><br/>É possível perceber quais partes do organismo registram algum desequilíbrio, e assim é possível reequilibrar as energias que circulam pelo corpo, reduzindo as tensões, provocando relaxamento e melhorando a circulação sanguínea.</p>"
         },
         {
             slug: "reiki", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Reiki", 
+            image: "reiki.jpeg",
+            description: "<p>Técnica complementar criada no Japão que consiste na imposição de mãos para a canalização da energia universal (da Fonte) para a energia individual do Ser. Esse encontro promove equilíbrio energético, harmonização e bem-estar.</p>"
         },
         {
             slug: "shiatsu", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
-        },
-        {
-            slug: "shiatsu-facial", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Shiatsu", 
+            image: "shiatsu.jpeg",
+            description: "<p>Essa massagem mistura técnicas manuais de massagem japonesa com a teoria dos meridianos da MTC (Medicina Tradicional Chinesa) segundo a qual existem linhas longitudinais pelo corpo que se conectam com pontos sensíveis ao toque. Esses pontos, ao serem estimulados, proporcionam uma sensação de conforto, tranquilidade e bem-estar.<br/><br/>O objetivo do shiatsu é perceber em quais pontos do corpo há acúmulo (jitsu) ou carência (kyo) de energia e atuar balanceando este fluxo energético a fim de reestabelecer a saúde física, emocional e espiritual do paciente.</p>"
         },
         {
             slug: "thai-massagem", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Thai Massagem", 
+            image: "thai-massagem.jpeg",
+            description: "<p>Massagem Ancestral Tailandesa, que traz um relaxamento profundo, pois opera diretamente sobre o sistema nervoso, acalmando a mente e colocando-a em alinhamento com a sabedoria interna que governa a vida.<br/><br/>Trabalha com alongamentos que aumentam a flexibilidade e melhoram a postura, estimulando a circulação sanguínea e reequilibrando o fluxo de energia.<br/><br/>É feita de roupa e é conduzida no solo, sob um tatame ou colchão fino.<br/><br/>A técnica é facilmente adaptada para todas as condições de saúde e é indicada para todos os tipos de pessoas.</p>"
         },
         {
             slug: "thetahealing", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "ThetaHealing", 
+            image: "thetahealing.jpeg",
+            description: "<p>Filosofia espiritual, que através das ondas cerebrais theta permite investigar o sistema de crenças, identificando crenças limitantes para que as mesmas possam ser reprogramadas por crenças fortalecedoras.<br/><br/>Com a técnica é possível trabalhar questões físicas, psicológicas e emocionais. Os assuntos a serem abordados vão desde o financeiro, passando pelos setores familiar, de relacionamentos, saúde, vícios, medos, ansiedade, depressão, insônia, insegurança, melhora no desempenho profissional e autoconfiança.</p>"
         },
         {
             slug: "ventosaterapia", 
-            title: "Título Exemplo 1", 
-            description: "Descrição <b>do</b> Exemplo 1"
+            title: "Ventosaterapia",
+            image: "ventosaterapia.jpg",
+            description: "<p>Tratamento natural no qual são usadas ventosas para melhorar a circulação sanguínea em um local do corpo. Para isso, as ventosas criam um efeito de vácuo, que suga a pele, resultando em um aumento do diâmetro dos vasos sanguíneo no exato local. Como resultado, existe uma maior oxigenação destes tecidos, permitindo a liberação de toxinas do sangue e do músculo com mais facilidade.</p>"
         }
-      ];
+    ];
 
-      // Atribui um evento de clique ao botão
-      $(".openModal").click(function(){
-        // Obtém o slug do botão clicado
+    $(".openModal").click(function() {
         var slug = $(this).data("slug");
-
-        // Obtém os dados correspondentes ao slug do JSON
         var data = findDataBySlug(slug);
 
-        // Atualiza o conteúdo do modal com os dados
         $("#modalLabel").text(data.title);
+        $("#modalImage").attr("src", './assets/img/terapias/' + data.image);
+        $("#modalImage").attr("alt", data.title);
+        $("#modalImage").attr("title", data.title);
         $("#modalDescription").html(data.description);
 
-        // Exibe o modal
         $("#modal").modal("show");
-      });
+    });
 
-      // Atribui um evento de fechamento ao modal
-      $("#modal").on("hidden.bs.modal", function () {
-        // Limpa o conteúdo do modal ao fechar
+    $("#modal").on("hidden.bs.modal", function () {
         $("#modalLabel").text("");
+        $("#modalImage").attr("src", '');
+        $("#modalImage").attr("alt", '');
+        $("#modalImage").attr("title", '');
         $("#modalDescription").html("");
-      });
+    });
 
-      $("#modal .close").click(function() {
+    $("#modal .close").click(function() {
         $("#modal").modal("hide");
-      });
+    });
 
-      // Atribui um evento de fechamento ao pressionar Esc
-      $(document).keydown(function(event) {
+    $(document).keydown(function(event) {
         if (event.key === "Escape") {
-          $("#modal").modal("hide");
+            $("#modal").modal("hide");
         }
-      });
+    });
 
-      // Função para encontrar dados no JSON pelo slug
-      function findDataBySlug(slug) {
-        // Encontra os dados no JSON pelo slug
+    function findDataBySlug(slug) {
         return jsonData.find(item => item.slug === slug);
-      }
+    }
 });
